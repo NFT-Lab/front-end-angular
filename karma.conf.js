7cd68beabde68a9c976909cb703b35b561a9eafa
Michele Baldisseri <michelebaldylello@gmail.com>
2021-06-09 21:21:52 +0200

    update for coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,9 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(
-        __dirname,
-        "./coverage/NFTLab-front-end-Angular"
-      ),
+      dir: require("path").join(__dirname, "./coverage"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "html" }, { type: "text-summary" }, { type: "lcov" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
