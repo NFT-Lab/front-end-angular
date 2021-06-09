2f5e8887b31878fea6ee2b6c757c34f519885e6c
Michele Baldisseri <michelebaldylello@gmail.com>
2021-06-09 20:55:50 +0200

    100% code coverage login form component

--- src/app/loginPage/login-form/login-form.component.spec.ts
+++ src/app/loginPage/login-form/login-form.component.spec.ts
@@ -1,16 +1,25 @@
 import { AppModule } from './../../app.module';
-import { MockAuthenticationService } from '@mocks/mockAuthentication.service';
 import { AuthenticationService } from '@service/authentication.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginFormComponent } from './login-form.component';
-import { throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
+import { User } from '@model/User';
 
 describe('Authentication with correct inputs', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let user: User = {
+    dob: new Date(),
+    name: 'test',
+    surname: 'test',
+    wallet: 'test',
+    email: 'test',
+  };
+  let email: any, password: any, loginButton: any;
+  let authService: AuthenticationService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,22 +33,22 @@ describe('Authentication with correct inputs', () => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('form should be valid with correct inputs', () => {
-    //set fields
-    let email = fixture.nativeElement.querySelector(
-        'input[formControlName=email]'
-      ),
-      password = fixture.nativeElement.querySelector(
-        'input[formControlName=password]'
-      ),
-      loginButton = fixture.nativeElement.querySelector('button[type=submit]');
+    //set form fields
+    email = fixture.nativeElement.querySelector('input[formControlName=email]');
+    password = fixture.nativeElement.querySelector(
+      'input[formControlName=password]'
+    );
+    loginButton = fixture.nativeElement.querySelector('button[type=submit]');
     //set input fields
     email.value = 'test@test.it';
     password.value = 'Test123@';
     email.dispatchEvent(new Event('input'));
     password.dispatchEvent(new Event('input'));
+    authService = fixture.debugElement.injector.get(AuthenticationService);
+  });
+
+  it('form should be valid with correct inputs', () => {
+    spyOn(authService, 'login').and.returnValue(of(user));
     //login
     loginButton.click();
     //expects
@@ -47,11 +56,30 @@ describe('Authentication with correct inputs', () => {
     expect(component.formGroup.controls.password.value).toBe('Test123@');
     expect(component.formGroup.valid).toBe(true);
   });
+
+  it('should show a specific error if server responds with error 400', () => {
+    spyOn(authService, 'login').and.returnValue(throwError({ status: 400 }));
+    //login
+    loginButton.click();
+    //expects
+    expect(component.formGroup.valid).toBe(true);
+    expect(component.errorMessage).toBe('Campi non compilati correttamente');
+  });
+
+  it('should show a specific error if server responds with error 204', () => {
+    spyOn(authService, 'login').and.returnValue(throwError({ status: 204 }));
+    //login
+    loginButton.click();
+    //expects
+    expect(component.formGroup.valid).toBe(true);
+    expect(component.errorMessage).toBe('Nessunca corrispondenza');
+  });
 });
 
 describe('Authentication with wrong inputs', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let email: any, password: any, loginButton: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -65,16 +93,16 @@ describe('Authentication with wrong inputs', () => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    //set form fields
+    email = fixture.nativeElement.querySelector('input[formControlName=email]');
+    password = fixture.nativeElement.querySelector(
+      'input[formControlName=password]'
+    );
+    loginButton = fixture.nativeElement.querySelector('button[type=submit]');
   });
 
   it('should show an error message for invalid input', () => {
-    let email = fixture.nativeElement.querySelector(
-        'input[formControlName=email]'
-      ),
-      password = fixture.nativeElement.querySelector(
-        'input[formControlName=password]'
-      ),
-      error = fixture.nativeElement.querySelector('#error');
+    let error = fixture.nativeElement.querySelector('#error');
     //set input fields
     email.value = 'test';
     password.value = 'test';
@@ -86,13 +114,6 @@ describe('Authentication with wrong inputs', () => {
   });
 
   it('should show a different error for every wrong input', () => {
-    let email = fixture.nativeElement.querySelector(
-        'input[formControlName=email]'
-      ),
-      password = fixture.nativeElement.querySelector(
-        'input[formControlName=password]'
-      ),
-      loginButton = fixture.nativeElement.querySelector('button[type=submit]');
     //set input fields for email error
     email.value = 'test';
     password.value = 'Test123@';
@@ -119,66 +140,5 @@ describe('Authentication with wrong inputs', () => {
       .toBe(`La password deve contere almeno 8 caratteri,
         una lettera minuscola e maiuscola ed un carattere speciale.`);
     expect(component.formGroup.valid).toBe(false);
-  });
-});
-
-describe('Authentication with correct inputs', () => {
-  let component: LoginFormComponent;
-  let fixture: ComponentFixture<LoginFormComponent>;
-  let email, password, loginButton: any;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [AuthenticationService],
-      imports: [HttpClientTestingModule, RouterTestingModule, AppModule],
-      declarations: [LoginFormComponent],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    //set fields
-    email = fixture.nativeElement.querySelector('input[formControlName=email]');
-    password = fixture.nativeElement.querySelector(
-      'input[formControlName=password]'
-    );
-    loginButton = fixture.nativeElement.querySelector('button[type=submit]');
-    //set input fields
-    email.value = 'test@test.it';
-    password.value = 'Test123@';
-    email.dispatchEvent(new Event('input'));
-    password.dispatchEvent(new Event('input'));
-  });
-  /*
-  it('form should be valid with correct inputs', () => {
-    //login
-    loginButton.click();
-    //expects
-    expect(component.formGroup.controls.email.value).toBe('test@test.it');
-    expect(component.formGroup.controls.password.value).toBe('Test123@');
-    expect(component.formGroup.valid).toBe(true);
-    expect(component.errorMessage).toBe('');
-  });*/
-
-  it('should show a specific error if server responds with error 400', () => {
-    const xService = fixture.debugElement.injector.get(AuthenticationService);
-    spyOn(xService, 'login').and.returnValue(throwError({ status: 400 }));
-    //login
-    loginButton.click();
-    //expects
-    expect(component.formGroup.valid).toBe(true);
-    expect(component.errorMessage).toBe('Campi non compilati correttamente');
-  });
-
-  it('should show a specific error if server responds with error 204', () => {
-    const xService = fixture.debugElement.injector.get(AuthenticationService);
-    spyOn(xService, 'login').and.returnValue(throwError({ status: 204 }));
-    //login
-    loginButton.click();
-    //expects
-    expect(component.formGroup.valid).toBe(true);
-    expect(component.errorMessage).toBe('Nessunca corrispondenza');
   });
 });
